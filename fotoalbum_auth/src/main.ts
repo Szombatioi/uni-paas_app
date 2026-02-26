6f7fbe3bf8385d085e8684a6c6a8d5fd6fb76ea7
Szombati Olivér <o.szombati@gmail.com>
2026-02-26 12:16:58 +0100

    Allowed Auth + Server CORS origins

--- fotoalbum_auth/src/main.ts
+++ fotoalbum_auth/src/main.ts
@@ -3,12 +3,13 @@ import { AppModule } from './app.module';
 import { SeederService } from './seed/seeder.service';
 
 async function bootstrap() {
-  console.log(__dirname)
+  // console.log(__dirname)
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: [
-      "http://localhost:3001"
+      "http://localhost:3001",
+      "https://fotoalbum-server.onrender.com"
     ], //any origin allowed
     credentials: true
   });
--- fotoalbum_server/src/main.ts
+++ fotoalbum_server/src/main.ts
@@ -5,7 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      "http://localhost:3000"
+      "http://localhost:3000",
+      "https://fotoalbum-frontend.onrender.com"
     ],
     credentials: true
   })
