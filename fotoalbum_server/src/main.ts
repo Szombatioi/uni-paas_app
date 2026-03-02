a9d9ba411cdedbed204a1d48bbf87788d56692ee
Szombati Olivér <o.szombati@gmail.com>
2026-03-02 14:44:29 +0100

    Server/Add Swagger API page

--- fotoalbum_server/src/main.ts
+++ fotoalbum_server/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,19 @@ async function bootstrap() {
     ],
     credentials: true
   })
+  
+  const config = new DocumentBuilder()
+    .setTitle('Fotoalbum Server API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  // Make "/" return the OpenAPI JSON document
+  app.getHttpAdapter().get('/', (req, res) => {
+    res.json(document);
+  });
+
   await app.listen(process.env.PORT ?? 3000, "0.0.0.0");
 }
 bootstrap();
