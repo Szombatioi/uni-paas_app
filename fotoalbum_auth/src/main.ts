b7282d36f4db505e8845e97f4ea9c418bd3c30f2
Szombati Olivér <o.szombati@gmail.com>
2026-03-02 14:43:33 +0100

    Auth/Add Swagger API page

--- fotoalbum_auth/src/main.ts
+++ fotoalbum_auth/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SeederService } from './seed/seeder.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   // console.log(__dirname)
@@ -16,6 +17,19 @@ async function bootstrap() {
 
   const seederService = app.get(SeederService);
   await seederService.seed();
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth service API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Make "/" return the OpenAPI JSON document
+  app.getHttpAdapter().get('/', (req, res) => {
+    res.json(document);
+  });
 
   await app.listen(process.env.PORT ?? 8888, "0.0.0.0");
 }
