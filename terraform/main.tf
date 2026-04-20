f21b9479482a5a2ab440df6230b89ffd9693051d
Szombati Olivér <o.szombati@gmail.com>
2026-04-20 13:23:01 +0200

    Removed imports for terraform - using Terraform Cloud state save instead

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,22 @@
 terraform {
+  cloud { #Now in the Terraform cloud instead of local state
+    organization = "fotoalbum"
+
+    workspaces {
+      name = "fotoalbum-openshift"
+    }
+  }
+
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.27"
     }
   }
-
-  # State tárolás GitHub Actions artifactként
-  backend "local" {
-    path = "terraform.tfstate"
-  }
 }
 
 provider "kubernetes" {
-  host  = var.openshift_api_url
-  token = var.openshift_token
+  host     = var.openshift_api_url
+  token    = var.openshift_token
   insecure = true
 }
